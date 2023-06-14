b0b5872806790dd29b68aadb9cefb15cfc205eec
Utkarsh Gupta <utkarsh.gupta1994@gmail.com>
2023-06-14 18:03:29 +0530

    completed times.js

--- 01-js/medium/times.js
+++ 01-js/medium/times.js
@@ -8,5 +8,15 @@ Hint - use Date class exposed in JS
 */
 
 function calculateTime(n) {
-    return 0.01;
+    
+    const startTime = new Date();
+    let sum = 0;
+    for(let i=0;i<n;i++){
+        sum +=i;
+    }
+    const endTime = new Date();
+    let finalTime = (endTime - startTime)/ 1000;
+    return finalTime;
 }
+
+ console.log(calculateTime(10000000000));
